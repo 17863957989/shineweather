f16ac3d31a69bc4d20ea9c4784d4d93f49a28dd1
LSG <570934880@qq.com>
2017-12-16 19:32:25 +0800

    加入创建数据库和表的各项配置。

---
+++ app/src/main/java/android/shineweather/com/shineweather/db/County.java
@@ -0,0 +1,44 @@
+package android.shineweather.com.shineweather.db;
+
+/**
+ * Created by LSG on 2017/12/16.
+ */
+
+public class County {
+    private int id;
+    private String countyName;
+    private String weatherID;
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public String getWeatherID() {
+        return weatherID;
+    }
+
+    public void setWeatherID(String weatherID) {
+        this.weatherID = weatherID;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+}
